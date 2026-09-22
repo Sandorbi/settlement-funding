59219da0fea5306492013560d92f6e53e3e8f3e8
Sandorbi <pichkhaiasandro@gmail.com>
2026-09-23 01:42:52 +0400

    Initial commit

---
+++ src/main/java/com/kursi/settlementfunding/SettlementFundingApplication.java
@@ -0,0 +1,13 @@
+package com.kursi.settlementfunding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettlementFundingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettlementFundingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kursi/settlementfunding/SettlementFundingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kursi.settlementfunding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettlementFundingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
